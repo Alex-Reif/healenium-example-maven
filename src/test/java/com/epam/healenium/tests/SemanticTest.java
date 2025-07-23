56794d47b7d3fdd73a0602ebfa592e534e654eac
Alex-Reif <69298932+Alex-Reif@users.noreply.github.com>
2025-07-23 16:05:56 +0300

    Healenium Report - 18 Jun 2025 - 13:12
    Updated locators in SemanticTest.java:
    - Replaced By.linkText('Change: LinkText, PartialLinkText') with By.cssSelector('a#change_links')
    - Replaced By.cssSelector('*[name=''change_name'']') with By.cssSelector('input#newName')
    - Replaced By.cssSelector('#change_id') with By.cssSelector('input#newValue')
    - Replaced By.partialLinkText('PartialLinkText') with By.cssSelector('a#change_links')
    - Replaced By.tagName('test_tag') with By.cssSelector('input#change_element')

--- src/test/java/com/epam/healenium/tests/SemanticTest.java
+++ src/test/java/com/epam/healenium/tests/SemanticTest.java
@@ -1,1 +1,91 @@
-updated_content_with_healed_locators
+package com.epam.healenium.tests;
+
+import com.epam.healenium.FrameworkPage;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.epam.healenium.constants.PagesType.TEST_ENV;
+
+public class SemanticTest extends BaseTest {
+
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Button click with find element by id")
+    public void testButtonClickWithId() {
+        FrameworkPage page = pages.get(TEST_ENV);
+
+        page.openPage();
+        driver.findElement(By.cssSelector("input#newValue"));
+        page.clickSubmitButton();
+        driver.findElement(By.cssSelector("input#newValue"));
+    }
+
+//    @Test
+//    @Severity(SeverityLevel.NORMAL)
+//    @Description("Find element by classname")
+//    public void testFindElementByClassName() {
+//        FrameworkPage mainPage = pages.get(String.valueOf(PagesType.MARKUP));
+//
+//        mainPage.openPage()
+//                .clickTestButton();
+//        mainPage.confirmAlert();
+//
+//        mainPage
+//                .generateMarkup()
+//                .clickTestButton(); //should be healed
+//        mainPage.confirmAlert();
+//
+//    }
+
+
+    @Test
+    @Severity(SeverityLevel.MINOR)
+    @Description("Find element by linktext")
+    public void testFindElementByLinkText() {
+        FrameworkPage page = pages.get(TEST_ENV);
+
+        page.openPage();
+        driver.findElement(By.cssSelector("a#change_links"));
+        page.clickSubmitButton();
+        driver.findElement(By.cssSelector("a#change_links"));
+    }
+
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Find element by name")
+    public void testFindElementByName() {
+        FrameworkPage page = pages.get(TEST_ENV);
+
+        page.openPage();
+        driver.findElement(By.cssSelector("input#newName"));
+        page.clickSubmitButton();
+        driver.findElement(By.cssSelector("input#newName"));
+    }
+
+    @Test
+    @Severity(SeverityLevel.MINOR)
+    @Description("Find element by partialLinkText")
+    public void testFindElementByPartialLinkText() {
+        FrameworkPage page = pages.get(TEST_ENV);
+
+        page.openPage();
+        driver.findElement(By.cssSelector("a#change_links"));
+        page.clickSubmitButton();
+        driver.findElement(By.cssSelector("a#change_links"));
+    }
+
+    @Test
+    @Severity(SeverityLevel.MINOR)
+    @Description("Find element by tagName")
+    public void testFindElementByTagName() {
+        FrameworkPage page = pages.get(TEST_ENV);
+
+        page.openPage();
+        driver.findElement(By.cssSelector("input#change_element")).isDisplayed();
+        page.clickSubmitButton();
+        driver.findElement(By.cssSelector("input#change_element")).isDisplayed();
+    }
+}
